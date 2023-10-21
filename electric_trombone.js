b42231a03e34a0184a7e683df4fae7bc98640893
Chihiro Kojima <43225316+chiiirotty@users.noreply.github.com>
2023-10-21 01:35:51 +0000

    first commit

---
+++ electric_trombone.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const Obniz = require("obniz");
+const obniz = new Obniz("");
+
+obniz.onconnect = async () => {
+    const hcsr04 = obniz.wired("HC-SR04", {gnd:0, echo:1, trigger:2, vcc:3});
+    // Javascript Example
+    var speaker = obniz.wired("Keyestudio_Buzzer", {signal:9, vcc:10, gnd:11});
+    
+    while(true) {
+        let avg = 0;
+        let count = 0;
+        for (let i=0; i<3; i++) { // measure three time. and calculate average
+          const val = await hcsr04.measureWait();
+          if (val) {
+            count++;
+            avg += val;
+          }
+        }
+        if (count > 1) {
+          avg /= count;
+        }
+        console.log(avg);
+        if (avg < 200) {
+            // ファ
+            speaker.play(698);
+        } else if (avg < 300) {
+            // ミ
+            speaker.play(659);
+        } else if (avg < 450) {
+            // レ
+            speaker.play(587);
+        } else if (avg < 600) {
+            // ド
+            speaker.play(523);
+        } else {
+            speaker.stop();
+        }
+        await obniz.wait(1000);
+    }
+}
